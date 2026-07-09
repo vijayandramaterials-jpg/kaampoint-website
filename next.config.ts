70ffcee0a86a23b336aeea379dd6eb80fd1000de
Rahul Pandya <rahulpandya16@gmail.com>
2026-07-10 01:00:51 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',      // यह लाइन प्रोजेक्ट को HTML/CSS/JS में बदल देगी
+  images: {
+    unoptimized: true,   // स्टेटिक एक्सपोर्ट में इमेजेस के लिए यह ज़रूरी है
+  },
 };
 
 export default nextConfig;
